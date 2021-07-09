3fcdea68badec65a64f1af7222748425afa5117d
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-09 22:45:29 +0300

    creates static file which handles images,css and javascript using special route

--- chat/models.py
+++ chat/models.py
@@ -6,4 +6,4 @@ class Image(models.Model):
     image_captions= models.CharField(max_length=60,blank=True,null=True)
     profile= models.ForeignKey(Profile,on_delete=models.CASCADE)
     likes= models.IntegerField(default=0)
-    comments=models.CharField(max_length=500)
+    comments=models.CharField(max_length=500) 
