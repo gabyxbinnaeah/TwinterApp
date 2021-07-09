ba6a385934cfd091f14aefba42c26b12f397daf4
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-09 22:41:38 +0300

    adds class image model with different fields

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image= models.ImageField(upload_to='pics',blank=True)
+    image_captions= models.CharField(max_length=60,blank=True,null=True)
+    profile= models.ForeignKey(Profile,on_delete=models.CASCADE)
+    likes= models.IntegerField(default=0)
+    comments=models.CharField(max_length=500)
