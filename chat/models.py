da5a6bac9913bd154207ee0f154638e2007ac969
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-10 09:11:06 +0300

    adds method that saves image model class

--- chat/models.py
+++ chat/models.py
@@ -5,12 +5,15 @@ class Image(models.Model):
     image= models.ImageField(upload_to='pics',blank=True)
     image_name = models.CharField(max_length=60,blank=True,null=True)
     image_captions= models.CharField(max_length=60,blank=True,null=True)
-    profile=models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
+    profile=models.ForeignKey('Profile',on_delete=models.CASCADE,null=True)
     likes= models.IntegerField(default=0)
     comments=models.CharField(max_length=500) 
 
     def __str__(self):
         return self.image_name
+
+    def save_image(self):
+        self.save()
 
 
 class Profile(models.Model):
