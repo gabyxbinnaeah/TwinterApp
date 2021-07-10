8741ae397ea6e47a337813df28d0e69c484fff7c
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-10 09:07:52 +0300

    creates profile model class with fields

--- chat/models.py
+++ chat/models.py
@@ -3,9 +3,14 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     image= models.ImageField(upload_to='pics',blank=True)
+    image_name = models.CharField(max_length=60,blank=True,null=True)
     image_captions= models.CharField(max_length=60,blank=True,null=True)
+    profile=models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
     likes= models.IntegerField(default=0)
     comments=models.CharField(max_length=500) 
+
+    def __str__(self):
+        return self.image_name
 
 
 class Profile(models.Model):
