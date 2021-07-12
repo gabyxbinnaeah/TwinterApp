2c947d17cd0d19faa3cd9763d885103b9294cb44
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-13 01:11:36 +0300

    creates the unfollow method

--- chat/views.py
+++ chat/views.py
@@ -15,12 +15,30 @@ def index(request):
     image_contents=Image.image_details()
     users = User.objects.exclude(id=request.user.id)
     return render(request, 'all-chat/home.html',{"image_contents":image_contents[::1],"users":users})
+
+def new_image(request):
     
-@login_required(login_url='/accounts/login/')
-def profile(request):
-    # currentUserId=request.user.id  currentUserId
-    profile_contents=Profile.profile_details()
-    return render(request, 'all-chat/home.html',{"profile_contents":profile_contents})
+    if request.method=='POST':
+        form=ImageForm(request.POST,request.FILES)
+        print(form.errors)
+        if form.is_valid():
+            posted_image=form.save(commit=False)
+            posted_image.user=request.user.profile
+            posted_image.save()
+
+            return redirect('homeUrl')
+
+    else:
+        form=ImageForm()
+
+    return render(request, 'all-chat/new_image.html',{"form":form}) 
+
+
+# @login_required(login_url='/accounts/login/')
+# def profile(request):
+#     # currentUserId=request.user.id  currentUserId
+#     profile_contents=Profile.profile_details()
+#     return render(request, 'all-chat/home.html',{"profile_contents":profile_contents})
 
 
 @login_required(login_url='/accounts/login/')
@@ -60,6 +78,49 @@ def update_profile(request):
 
 
 @login_required(login_url='/accounts/login/')
+def search_profile(request):
+    if 'search_user' in request.GET and request.GET['search_user']:
+        name = request.GET.get("search_user")
+        results = Profile.search_profile(name)
+        print(results)
+        message = f'name'
+        params = {
+            'results': results,
+            'message': message
+        }
+        return render(request, 'results.html', params)
+    else:
+        message = "Kindly make selection"
+    return render(request, 'results.html', {'message': message})
+
+
+
+@login_required(login_url='/accounts/login/')
+def user_profile(request, username):
+    userProfile = get_object_or_404(User, username=username)
+    if request.user == userProfile:
+        return redirect('profile', username=request.user.username)
+    user_posts = userProfile.profile.images.all()
+    
+    followers = Follow.objects.filter(followed=userProfile.profile)
+    follow_status = None
+    for follower in followers:
+        if request.user.profile == follower.follower:
+            follow_status = True
+        else:
+            follow_status = False
+    params = {
+        'userProfile': userProfile,
+        'user_posts': user_posts,
+        'followers': followers,
+        'follow_status': follow_status
+    }
+    return render(request, 'user_profile.html', params)
+
+
+
+
+@login_required(login_url='/accounts/login/')
 def follow(request, to_follow):
     if request.method == 'GET':
         next_user_profile = Profile.objects.get(pk=to_follow)
@@ -68,29 +129,39 @@ def follow(request, to_follow):
         return redirect('user_profile', next_user_profile.user.username)
 
 
+@login_required(login_url='/accounts/login/')
+def unfollow(request, to_unfollow):
+    if request.method == 'GET':
+        secondUser = Profile.objects.get(pk=to_unfollow)
+        unfollow_d = Follow.objects.filter(follower=request.user.profile, followed=secondUser)
+        unfollow_d.delete()
+        return redirect('user_profile', secondUser.user.username)
 
 
 
-
-
-
-
-def new_image(request):
-    
-    if request.method=='POST':
-        form=ImageForm(request.POST,request.FILES)
-        print(form.errors)
+@login_required(login_url='/accounts/login/')
+def comment(request, id):
+    image = get_object_or_404(Image, pk=id)
+    comments = image.comment.all()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
         if form.is_valid():
-            posted_image=form.save(commit=False)
-            posted_image.user=request.user.profile
-            posted_image.save()
-
-            return redirect('homeUrl')
-
+            comment = form.save(commit=False)
+            comment.photo = image
+            comment.user = request.user.profile
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
     else:
-        form=ImageForm()
+        form = CommentForm()
+    params = {
+        'image': image,
+        'form': form,
+        'comments':comments,
+    }
+    return render(request, 'post.html', params)
 
-    return render(request, 'all-chat/new_image.html',{"form":form}) 
+
+
 
           
             
