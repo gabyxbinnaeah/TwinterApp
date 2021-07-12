0ef713ad791201903039dcfc5e83c2602c7ba845
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-12 23:31:54 +0300

    imports signal

--- chat/models.py
+++ chat/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User 
 import cloudinary
 from cloudinary.models import CloudinaryField
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 class Image(models.Model):
     image = cloudinary.models.CloudinaryField('pics',null=True, blank=True)
@@ -47,22 +48,45 @@ class Profile(models.Model):
     profile_photo= cloudinary.models.CloudinaryField('pics',null=True, blank=True)
     bio= models.TextField(max_length=1000,blank=True,null=True)
     user= models.OneToOneField(User,related_name="profile",null=True,on_delete=models.CASCADE) 
+    name=models.CharField(max_length=40,blank=True,null=True)
 
 
     def __str__(self):
-        return self.bio
+        return self.name
 
     def save_profile(self):
         '''
          Defines method that saves profile class model
         '''
         self.save()
+
     @classmethod
     def profile_details(cls):
         details_container=cls.objects.all() 
         return details_container
         
-        # user=currentUser
+    def photo_url(self):
+        if self.profile_photo and hasattr(self.profile_photo, 'url'):
+            return self.profile_photo.url 
+
+    @receiver(post_save, sender=User)  
+    def create_profile_user(sender, instance, created, **kwargs):
+        '''
+        method that creates user profile 
+        '''
+        try:
+            instance.profile.save()
+
+        except ObjectDoesNotExist:
+            Profile.objects.create(user=instance)
+
+
+    @receiver(post_save, sender=User)
+    def save_profile_user(sender, instance, **kwargs):
+        '''
+        method that saves user profile 
+        '''
+        instance.profile.save()
 
 
     def delete_profile(self):
@@ -79,19 +103,24 @@ class Profile(models.Model):
         '''
         return cls.objects.filter(id=id).update(bio=bio) 
 
-
+    @classmethod
+    def search_profile(cls, name):
+        '''
+        method that filters user profile by name
+        '''
+        return cls.objects.filter(user_username_icontains=name).all()
 
 class Follow(models.Model):
     follower=models.ForeignKey('Profile',on_delete=models.CASCADE,related_name='follower', null=True,blank=True)
     following=models.ForeignKey('Profile',related_name='following',on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.follower
+        return f'{self.follower} Follow'
 
 class Comment(models.Model):
     user=models.ForeignKey('Profile',related_name='comment',on_delete=models.CASCADE)
     comment=models.TextField()
-    photo = models.ForeignKey('Image',related_name='photoComments',  on_delete=models.CASCADE)
+    photo = models.ForeignKey('Image',related_name='comment',  on_delete=models.CASCADE)
 
     class Meta:
         ordering=["-pk"]
